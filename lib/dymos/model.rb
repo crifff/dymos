81ec367d81c6275a9d437fb77a572a68f19027c3
criff <hoshina85@gmail.com>
2014-08-21 22:00:01 +0900

    remove config

--- lib/dymos/model.rb
+++ lib/dymos/model.rb
@@ -90,10 +90,6 @@ module Dymos
 
     def dynamo
 
-      Aws.config[:region] = 'us-west-1'
-      Aws.config[:endpoint] = 'http://localhost:4567'
-      Aws.config[:access_key_id] = 'XXX'
-      Aws.config[:secret_access_key] = 'XXX'
       @client ||= Aws::DynamoDB::Client.new
     end
 

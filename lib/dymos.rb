57733e88e5af47cea9e92e7d2735026d77e9de1e
criff <hoshina85@gmail.com>
2014-09-02 22:37:43 +0900

    require

--- lib/dymos.rb
+++ lib/dymos.rb
@@ -1,12 +1,11 @@
-require "dymos/model"
+require "dymos/attribute"
 require "dymos/command"
+require "dymos/model"
 require "dymos/version"
+require "dymos/query/attribute"
+require "dymos/query/builder"
+require "dymos/query/expect"
+require "dymos/query/put_item"
 
 module Dymos
-  module Query
-
-  end
-  def self.model
-    Dymos::Model
-  end
 end

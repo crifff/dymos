78c01ff20d4fcee83e890c5f71ad43141b8bae1b
criff <hoshina85@gmail.com>
2014-09-02 22:45:44 +0900

    依存の順番に並べ替え

--- lib/dymos.rb
+++ lib/dymos.rb
@@ -1,11 +1,11 @@
+require "dymos/query/attribute"
+require "dymos/query/expect"
+require "dymos/query/builder"
+require "dymos/query/put_item"
 require "dymos/attribute"
 require "dymos/command"
 require "dymos/model"
 require "dymos/version"
-require "dymos/query/attribute"
-require "dymos/query/builder"
-require "dymos/query/expect"
-require "dymos/query/put_item"
 
 module Dymos
 end

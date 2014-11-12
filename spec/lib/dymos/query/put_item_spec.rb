4cd677d4ee59a3e0a36b0ad62a7206be11104229
criff <hoshina85@gmail.com>
2014-11-12 18:56:57 +0900

    put_item

--- spec/lib/dymos/query/put_item_spec.rb
+++ spec/lib/dymos/query/put_item_spec.rb
@@ -1,319 +1,63 @@
 describe Dymos::Query::PutItem do
-  before :all do
-    Aws.config[:region] = 'us-west-1'
-    Aws.config[:endpoint] = 'http://localhost:4567'
-    Aws.config[:access_key_id] = 'XXX'
-    Aws.config[:secret_access_key] = 'XXX'
+  describe 'build query', order: :defined do
+    let(:builder) { Dymos::Query::PutItem.new }
+    let(:table) { 'test_put_item_table' }
 
-    client = Aws::DynamoDB::Client.new
-    client.delete_table(table_name: 'test_put_item') if client.list_tables[:table_names].include?('test_put_item')
-    client.create_table(
-        table_name: 'test_put_item',
-        attribute_definitions: [
-            {attribute_name: 'id', attribute_type: 'S'},
-        ],
-        key_schema: [
-            {attribute_name: 'id', key_type: 'HASH'}
-        ],
-        provisioned_throughput: {
-            read_capacity_units: 1,
-            write_capacity_units: 1,
-        })
-    client.put_item(table_name: 'test_put_item', item: {id: 'hoge', name: '太郎'})
-    client.put_item(table_name: 'test_put_item', item: {id: 'fuga', category_id: 1})
-
-    class TestItem < Dymos::Model
-      table :test_put_item
-      field :id, :string
-      field :name, :string
-    end
-  end
-
-  let(:client) { Aws::DynamoDB::Client.new }
-  describe :put_item do
-    describe "クエリ生成" do
-      it "追加のみ" do
-        query = TestItem.put.item(id: "foo", name: "John")
-        expect(query.query).to eq({
-                                      table_name: "test_put_item",
-                                      item: {id: "foo", name: "John"},
-                                      return_values: "ALL_OLD",
-                                  })
-      end
-
-    end
-    it "条件なしput_item実行 追加時はattributesがnilになる" do
-      query = TestItem.put.item(id: "foo", name: "John")
-      result = query.execute client
-      expect(result).to eq(nil)
+    it do
+      @client.delete_table(table_name: table) if @client.list_tables[:table_names].include?(table)
+      query=Dymos::Query::CreateTable.new.name(table).attributes(id: 'S').keys(id: 'HASH').build
+      @client.create_table(query)
+      @client.put_item(table_name: table, item: {id: 'hoge', param1: 101, param2: 201, param3: "hoge", param4: [1, 0, 1]})
+      @client.put_item(table_name: table, item: {id: 'fuga', param1: 102, param2: 202, param3: "fuga", param4: [1, 0, 2]})
+      @client.put_item(table_name: table, item: {id: 'piyo', param1: 103, param2: 203, param3: "piyo", param4: [1, 0, 3]})
     end
 
-    # it "条件ありput_item" do
-    #   query = TestPutItem.put.item(id: "hoge", name: "次郎").expected(name: "== 太郎")
-    #   expect(query.query).to eq({
-    #                                 table_name: "test_put_item",
-    #                                 item: {id: "hoge", name: "次郎"},
-    #                                 :expected => {:name => {:value => "太郎", :comparison_operator => "EQ"}},
-    #                                 return_values: "ALL_OLD",
-    #                             })
-    #
-    #   query = TestPutItem.put.item(id: "hoge", name: "次郎").expected(category_id: "== 1")
-    #   expect(query.query).to eq({
-    #                                 table_name: "test_put_item",
-    #                                 item: {id: "hoge", name: "次郎"},
-    #                                 :expected => {:category_id => {:value => 1, :comparison_operator => "EQ"}},
-    #                                 return_values: "ALL_OLD",
-    #                             })
-    #
-    #   query = TestPutItem.put.item(id: "hoge", name: "次郎").expected(category_id: "between 0 2")
-    #   expect(query.query).to eq({
-    #                                 table_name: "test_put_item",
-    #                                 item: {id: "hoge", name: "次郎"},
-    #                                 :expected => {:category_id => {:attribute_value_list => [0, 2], :comparison_operator => "BETWEEN"}},
-    #                                 return_values: "ALL_OLD",
-    #                             })
-    # end
+    it :create do
+      data={id: 'poyo', param1: 104, param2: 204, param3: "poyo", param4: [1, 0, 4]}
+      builder.name(table).item(data).return_values(:ALL_OLD)
+      result = @client.put_item builder.build
+      expect(result.error).to eq(nil)
+      expect(result.attributes).to eq(nil)
 
-    it "条件ありput_item実行 成功すると古いデータを返す" do
-      query = TestItem.put.item(id: "hoge", name: "次郎").expected(name: "== 太郎")
-      result = query.execute client
-      expect(result.attributes).to eq({id: "hoge", name: "太郎"})
-      expect(result.metadata).to eq(consumed_capacity: nil, item_collection_metrics: nil)
+      res = @client.get_item(table_name: table, key: {id: 'poyo'})
+      expect(res.item.to_hash).to eq(data.deep_stringify_keys)
     end
 
-    describe "条件指定" do
-      it "undefined operator" do
-        expect { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "= 1") }.to raise_error(ArgumentError)
-      end
-
-      describe :== do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "== 1") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:value => 1, :comparison_operator => "EQ"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-          expect(result.metadata).to eq(consumed_capacity: nil, item_collection_metrics: nil)
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "== 2")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :!= do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "!= 0") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:value => 0, :comparison_operator => "NE"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "== 2")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :> do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "> 0") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:value => 0, :comparison_operator => "GT"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "> 2")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :>= do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: ">= 1") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:value => 1, :comparison_operator => "GE"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "> 2")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :< do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "< 2") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:value => 2, :comparison_operator => "LT"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "< 0")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :<= do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "<= 1") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:value => 1, :comparison_operator => "LE"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "<= 0")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :between do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "between 1 2") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:category_id => {:attribute_value_list => [1, 2], :comparison_operator => "BETWEEN"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(category_id: "between 2 3")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :contain do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(id: "contains uga") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:id => {:value => "uga", :comparison_operator => "CONTAINS"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(id: "contains ppp")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :not_contain do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(id: "not_contains ppp") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:id => {:value => "ppp", :comparison_operator => "NOT_CONTAINS"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(id: "not_contains uga")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :begins_with do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(id: "begins_with fug") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:id => {:value => "fug", :comparison_operator => "BEGINS_WITH"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(id: "begins_with uga")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :is_null do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(bar: "is_null") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:bar => {:comparison_operator => "NULL"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(id: "is_null")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
-
-      describe :is_not_null do
-        let(:query) { TestItem.put.item(id: "fuga", category_id: 1).expected(id: "is_not_null") }
-        it :query do
-          expect(query.query).to eq(table_name: "test_put_item",
-                                    item: {id: "fuga", category_id: 1},
-                                    expected: {:id => {:comparison_operator => "NOT_NULL"}},
-                                    return_values: "ALL_OLD")
-        end
-        it :execute do
-          result = query.execute client
-          expect(result.attributes).to eq({id: "fuga", category_id: 1})
-        end
-        it :error do
-          query = TestItem.put.item(id: "fuga", category_id: 1).expected(bar: "is_not_null")
-          expect(query.execute(client)).to eq(false)
-        end
-      end
+    it 'failed expect' do
+      data={id: 'poyo', param1: 104+1, param2: 204, param3: "poyo", param4: [1, 0, 4]<<'x'}
+      builder.name(table).item(data).return_values(:ALL_OLD).expected(param1: 0)
+      expect { @client.put_item(builder.build) }.to raise_error(Aws::DynamoDB::Errors::ConditionalCheckFailedException)
     end
-    #
-    # it :< do
-    #   query = TestPutItem.put.item(id: "fuga", category_id: 1).expected(category_id: "< 2")
-    #   result = query.execute client
-    #   expect(result.attributes).to eq({id: "fuga", category_id: 1})
-    # end
-    # it :between do
-    #   query = TestPutItem.put.item(id: "fuga", category_id: 1).expected(category_id: "between 1 2")
-    #   result = query.execute client
-    #   expect(result.attributes).to eq({id: "fuga", category_id: 1})
-    # end
+
+    it :expected do
+      data={id: 'poyo', param1: 104+1, param2: 204, param3: "poyo", param4: [1, 0, 4]<<'x'}
+      builder.name(table).item(data).return_values(:ALL_OLD).expected(param1: 104, param4: [1, 0, 4])
+      result = @client.put_item(builder.build)
+      expect(result.error).to eq(nil)
+      expect(result.attributes["param1"]).to eq(104)
+
+
+      res = @client.get_item(table_name: table, key: {id: 'poyo'})
+      expect(res.item["param1"]).to eq(105)
+    end
+
+    it :between do
+      data={id: 'poyo', param1: 105+1, param2: 204, param3: "poyo", param4: [1, 0, 4, 'x']}
+      builder.name(table).item(data).return_values(:ALL_OLD).expected([[:param1, :BETWEEN, [104, 106]]])
+
+      result = @client.put_item(builder.build)
+      expect(result.error).to eq(nil)
+      expect(result.attributes["param1"]).to eq(105)
+    end
+
+    it :in do
+      data={id: 'poyo', param1: 106+1, param2: 204, param3: "poyo", param4: [1, 0, 4, 'x']}
+      builder.name(table).item(data).return_values(:ALL_OLD).add_expected(:param1, :IN, [101, 103, 106])
+
+      result = @client.put_item(builder.build)
+      expect(result.error).to eq(nil)
+      expect(result.attributes["param1"]).to eq(106)
+    end
   end
 end
 
